2e5405f45dd45a524051553e3c42a7b6baa7515e
Safin <ilnursafin35@gmail.com>
2026-09-05 12:50:19 +0500

    September 5th, if/elif/else

--- littleLessons.sh
+++ littleLessons.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-if pwd
-then
-echo "It works"
+fileName="kitty.txt"
+touch $(pwd)/$fileName
+var1="mysql"
+if grep keksql /etc/passwd
+then echo "$var1 exists"
+elif
+echo "writing an another sql engineer to the file $(pwd)/$fileName"
+echo "writing is ended with succeed"
+then echo "Haidaric" > $(pwd)/$fileName
 fi
