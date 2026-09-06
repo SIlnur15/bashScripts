4374e87150b25f6ece6ca5cef5aa7faee47d4dd7
Safin <ilnursafin35@gmail.com>
2026-09-06 13:57:27 +0500

    Sunday 6th September, comparing

--- littleLessons.sh
+++ littleLessons.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-fileName="kitty.txt"
-touch $(pwd)/$fileName
-var1="mysql"
-if grep keksql /etc/passwd
-then echo "$var1 exists"
-elif
-echo "writing an another sql engineer to the file $(pwd)/$fileName"
-echo "writing is ended with succeed"
-then echo "Haidaric" > $(pwd)/$fileName
+if [[ 5 -gt 15 ]]
+then echo "hello HAidaric"
+else echo "fatality"
 fi
