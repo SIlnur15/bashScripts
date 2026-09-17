9be398a786a70b71681ad1a34cf70c5ef581de68
Safin <ilnursafin35@gmail.com>
2026-09-18 01:46:06 +0500

    September 18th, curl

--- littleLessons.sh
+++ littleLessons.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Postman"
+API_URL="https://api.example.com/users"
+TOKEN="YOUR_ACCESS_TOKEN"
+
+# GET-запрос
+curl -H "Authorization: Bearer $TOKEN" "$API_URL" | jq .  # jq для красивого вывода JSON
+
+# POST-запрос
+curl -X POST "$API_URL" \
+  -H "Content-Type: application/json" \
+  -d '{"name": "John", "age": 30}'
